6fee7dca44efbfc51ab61fce03424e0b856a3d40
Akashkewat12 <akashbind.bca.15@gmail.com>
2025-04-13 20:45:17 +0530

    adding

---
+++ mvcPracticeWeek2/mvcPracticeWeek2/src/main/java/com/practice/mvcPracticeWeek2/MvcPracticeWeek2Application.java
@@ -0,0 +1,13 @@
+package com.practice.mvcPracticeWeek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPracticeWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPracticeWeek2Application.class, args);
+	}
+
+}
